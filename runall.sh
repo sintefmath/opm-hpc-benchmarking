7821f8f1b7465b7e33de9eb8fd3da9481981ccac
Franz Georg Fuchs <franzgeorgfuchs@gmail.com>
2020-02-29 20:12:26 +0100

    updated bash script and gitignore

--- runall.sh
+++ runall.sh
@@ -1,21 +1,136 @@
 #!/bin/bash
 
-maxproc=6
+###########################
+# 1) parse arguments
+###########################
 
-for data in "spe9/SPE9.DATA" "norne/NORNE_ATW2013.DATA"
+maxproc=""
+flowdir=""
+cases=""
+maxthreads=""
+
+count=0
+args=(${@})
+argsleft=$#
+
+if [ $# == 0 ]; then
+    echo "please specify the maximum number of precessors with --maxproc";
+    exit 1
+else
+    while [[ $argsleft -gt 0 ]]; do
+        arg=${args[count]}
+        (( count++ ))
+        (( argsleft-- ))
+
+        case $arg in
+        --maxproc)
+            if [[ $argsleft -gt 0 ]]; then
+                if   [ ${args[count]} == "--maxthreads" ] \
+                    || [ ${args[count]} == "--flowdir" ] \
+                    || [ ${args[count]} == "--data" ] \
+                ; then
+                    break;
+                else
+                    maxproc=${args[count]}
+                    (( count++ ))
+                    (( argsleft-- ))
+                fi
+            fi
+            ;;
+        --maxthreads)
+            if [[ $argsleft -gt 0 ]]; then
+                if   [ ${args[count]} == "--maxproc" ] \
+                    || [ ${args[count]} == "--flowdir" ] \
+                    || [ ${args[count]} == "--data" ] \
+                ; then
+                    break;
+                else
+                    maxthreads=${args[count]}
+                    (( count++ ))
+                    (( argsleft-- ))
+                fi
+            fi
+            ;;
+        --flowdir)
+            if [[ $argsleft -gt 0 ]]; then
+                if   [ ${args[count]} == "--maxproc" ] \
+                    || [ ${args[count]} == "--maxthreads" ] \
+                    || [ ${args[count]} == "--data" ] \
+                ; then
+                    break;
+                else
+                    flowdir=${args[count]}
+                    (( count++ ))
+                    (( argsleft-- ))
+                fi
+            fi
+            ;;
+        --data)
+            while [[ $argsleft -gt 0 ]]; do
+                if   [ ${args[count]} == "--maxproc" ] \
+                    || [ ${args[count]} == "--maxthreads" ] \
+                    || [ ${args[count]} == "--flowdir" ] \
+                ; then
+                    break;
+                else
+                    cases+=" "${args[count]}
+                    (( count++ ))
+                    (( argsleft-- ))
+                fi
+            done
+            ;;
+        *)
+            echo "unknown option:" $arg;
+            ;;
+        esac
+        shift # past argument or value
+    done
+fi
+
+if [[ $maxproc == "" ]]; then
+    echo "please specify the maximum number of precessors with --maxproc";
+    exit 1
+fi
+
+if [[ $maxthreads == "" ]]; then
+    echo "please specify the maximum number of threads with --maxthreads";
+    exit 1
+fi
+
+if [[ $flowdir == "" ]]; then
+    echo "please specify the flow directory with --flowdir"
+    exit 1
+fi
+
+if [[ $cases == "" ]]; then
+    echo "please specify the cases you want to run with --data, e.g., --data dir1/spe9/SPE9.DATA dir2/norne/NORNE_ATW2013.DATA"
+    exit 1
+fi
+
+re='^[0-9]+$'
+if ! [[ $maxproc =~ $re ]] ; then
+    echo "maxproc is not a number" >&2
+    exit 1
+fi
+
+if ! [[ $maxthreads =~ $re ]] ; then
+    echo "maxthreads is not a number" >&2
+    exit 1
+fi
+
+if [[ ! -d $flowdir ]]; then
+    echo "flowdir is not a directory" >&2
+    exit 1
+fi
+
+for data in $cases
 do
-    if [[ "$data" == "spe9/SPE9.DATA" ]]
-    then
-        casename="spe9"
-    elif [[ "$data" == "norne/NORNE_ATW2013.DATA" ]]
-    then
-        casename="norne"
-    fi
+    casename="$(basename -s .DATA $data)"
     for numproc in $(seq $maxproc)
     do
-        for numthreads in 1 2
+        for numthreads in $(seq $maxthreads)
         do
-            mpirun -np $numproc ~/opm/opm-simulators/build/bin/flow ~/opm/opm-data/$data --threads-per-process=$numthreads --output-dir=$casename"_processors"$numproc"_threads"$numthreads
+            mpirun -np $numproc $flowdir/flow ~/opm/opm-data/$data --threads-per-process=$numthreads --output-dir=$casename"_processors"$numproc"_threads"$numthreads
         done
     done
 done
