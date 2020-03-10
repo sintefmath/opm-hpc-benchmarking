253f176d44d714afa2ce2f8e992dd3a7842521eb
Franz G. Fuchs <franz.fuchs@sintef.no>
2020-03-10 10:56:56 +0100

    increase robustness

--- parseall.py
+++ parseall.py
@@ -2,6 +2,8 @@ import sys
 if sys.version_info < (3, 5):
     raise "must use python 3.5 or greater"
 import os
+import os.path
+from os import path
 
 import subprocess
 import numpy as np
@@ -9,76 +11,105 @@ import numpy as np
 def parse(command):
     return subprocess.run([command, '-l'], stdout=subprocess.PIPE, shell=True).stdout.decode('utf-8')
 
+def getdebugfile(casename, numthreads, numprocessors):
+    return "sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)+'/'+casename+'.DBG'
+
 def get_totalTime(casename, numthreads, maxproc):
-    res=np.zeros(maxproc)
+    res=np.empty(maxproc)
+    res[:] = np.nan
     for numprocessors in np.arange(1,maxproc+1):
-        dn="sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)
-        command='tail -n30 '+dn+'/*DBG|grep "Total time (seconds)"|cut -d ":" -f 2'
-        res[numprocessors-1] = float(parse(command))
+        dbgf=getdebugfile(casename,numthreads, numprocessors)
+        if path.exists(dbgf):
+            command='tail -n30 '+dbgf+'|grep "Total time (seconds)"|cut -d ":" -f 2'
+            tmp=parse(command).strip()
+            if tmp:
+                res[numprocessors-1] = float(tmp)
     return res
 
 def get_numNewtonIterations(casename, numthreads, maxproc):
-    res=np.zeros(maxproc)
+    res=np.empty(maxproc)
+    res[:] = np.nan
     for numprocessors in np.arange(1,maxproc+1):
-        dn="sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)
-        command='tail -n30 '+dn+'/*DBG|grep "Newton Iterations"|cut -d ":" -f 2|cut -d " " -f 5'
-        res[numprocessors-1] = int(parse(command))
+        dbgf=getdebugfile(casename,numthreads, numprocessors)
+        if path.exists(dbgf):
+            command='tail -n30 '+dbgf+'|grep "Newton Iterations"|cut -d ":" -f 2|cut -d " " -f 5'
+            tmp=parse(command).strip()
+            if tmp:
+                res[numprocessors-1] = int(tmp)
     return res
 
 def get_numLinearIterations(casename, numthreads, maxproc):
-    res=np.zeros(maxproc)
+    res=np.empty(maxproc)
+    res[:] = np.nan
     for numprocessors in np.arange(1,maxproc+1):
-        dn="sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)
-        command='tail -n30 '+dn+'/*DBG|grep "Linear Iterations"|cut -d ":" -f 2|cut -d " " -f 5'
-        res[numprocessors-1] = int(parse(command))
+        dbgf=getdebugfile(casename,numthreads, numprocessors)
+        if path.exists(dbgf):
+            command='tail -n30 '+dbgf+'|grep "Linear Iterations"|cut -d ":" -f 2|cut -d " " -f 5'
+            tmp=parse(command).strip()
+            if tmp:
+                res[numprocessors-1] = int(tmp)
     return res
 
 def get_AssemplyTime(casename, numthreads, maxproc):
-    res=np.zeros(maxproc)
+    res=np.empty(maxproc)
+    res[:] = np.nan
     for numprocessors in np.arange(1,maxproc+1):
-        dn="sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)
-        command='tail -n30 '+dn+'/*DBG|grep "Assembly time (seconds)"|cut -d ":" -f 2|cut -d " " -f 6'
-        res[numprocessors-1] = float(parse(command))
+        dbgf=getdebugfile(casename,numthreads, numprocessors)
+        if path.exists(dbgf):
+            command='tail -n30 '+dbgf+'|grep "Assembly time (seconds)"|cut -d ":" -f 2|cut -d " " -f 6'
+            tmp=parse(command).strip()
+            if tmp:
+                res[numprocessors-1] = float(tmp)
     return res
 
 def get_linearSolveTime(casename, numthreads, maxproc):
-    res=np.zeros(maxproc)
+    res=np.empty(maxproc)
+    res[:] = np.nan
     for numprocessors in np.arange(1,maxproc+1):
-        dn="sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)
-        command='tail -n30 '+dn+'/*DBG|grep "Linear solve time (seconds)"|cut -d ":" -f 2|cut -d " " -f 2'
-        res[numprocessors-1] = float(parse(command))
+        dbgf=getdebugfile(casename,numthreads, numprocessors)
+        if path.exists(dbgf):
+            command='tail -n30 '+dbgf+'|grep "Linear solve time (seconds)"|cut -d ":" -f 2|cut -d " " -f 2'
+            tmp=parse(command).strip()
+            if tmp:
+                res[numprocessors-1] = float(tmp)
     return res
 
 def get_numCells(casename, numthreads, maxproc):
-    owned=np.zeros((maxproc,maxproc))
-    overlap=np.zeros((maxproc,maxproc))
-    total=np.zeros((maxproc,maxproc))
+    owned=np.empty((maxproc,maxproc))
+    owned[:] = np.nan
+    overlap=np.empty((maxproc,maxproc))
+    overlap[:] = np.nan
+    total=np.empty((maxproc,maxproc))
+    total[:] = np.nan
     for numprocessors in np.arange(2,maxproc+1):
-        dn="sims/"+casename+"_processors"+str(numprocessors)+"_threads"+str(numthreads)
-        if numprocessors == 2:
-            command = 'grep -i load '+dn+'/*DBG|cut -d " " -f 4'
-            totalactivecells = int(parse(command))
-            total[0,0] = totalactivecells
-        command='line=`grep -i load '+dn+'/*DBG -n`; linenumber=`echo $line|cut -d ":" -f 1`; linenumber=`expr $linenumber + 3`; echo $linenumber'
-        linenumber = str(parse(command)).strip()
-        #owned
-        rest=str(1)
-        command='data=`tail -n+'+linenumber+' '+dn+'/*DBG | head -n'+str(numprocessors)+'`; count=0;for d in $data; do if [ `expr $count % 4` -eq '+rest+' ]; then echo $d", "; fi; count=`expr $count + 1`; done'
-        data = str(parse(command))
-        for i in np.arange(0,numprocessors):
-            owned[numprocessors-1,i] = int(data.split(', ')[i])
-        #overlap
-        rest=str(2)
-        command='data=`tail -n+'+linenumber+' '+dn+'/*DBG | head -n'+str(numprocessors)+'`; count=0;for d in $data; do if [ `expr $count % 4` -eq '+rest+' ]; then echo $d", "; fi; count=`expr $count + 1`; done'
-        data = str(parse(command))
-        for i in np.arange(0,numprocessors):
-            overlap[numprocessors-1,i] = int(data.split(', ')[i])
-        #total
-        rest=str(3)
-        command='data=`tail -n+'+linenumber+' '+dn+'/*DBG | head -n'+str(numprocessors)+'`; count=0;for d in $data; do if [ `expr $count % 4` -eq '+rest+' ]; then echo $d", "; fi; count=`expr $count + 1`; done'
-        data = str(parse(command))
-        for i in np.arange(0,numprocessors):
-            total[numprocessors-1,i] = int(data.split(', ')[i])
+        dbgf=getdebugfile(casename,numthreads, numprocessors)
+        if path.exists(dbgf):
+            if numprocessors == 2:
+                command = 'grep -i load '+dbgf+'|cut -d " " -f 4'
+                tmp = parse(command)
+                if tmp:
+                    total[0,0] = int(tmp)
+            command='line=`grep -i load '+dbgf+' -n`; linenumber=`echo $line|cut -d ":" -f 1`; linenumber=`expr $linenumber + 3`; echo $linenumber'
+            linenumber = str(parse(command)).strip()
+            if int(linenumber)>3:### if the line is not found, linenumber will be 3
+                #owned
+                rest=str(1)
+                command='data=`tail -n+'+linenumber+' '+dbgf+' | head -n'+str(numprocessors)+'`; count=0;for d in $data; do if [ `expr $count % 4` -eq '+rest+' ]; then echo $d", "; fi; count=`expr $count + 1`; done'
+                data = str(parse(command))
+                for i in np.arange(0,numprocessors):
+                    owned[numprocessors-1,i] = float(data.split(', ')[i])
+                #overlap
+                rest=str(2)
+                command='data=`tail -n+'+linenumber+' '+dbgf+' | head -n'+str(numprocessors)+'`; count=0;for d in $data; do if [ `expr $count % 4` -eq '+rest+' ]; then echo $d", "; fi; count=`expr $count + 1`; done'
+                data = str(parse(command))
+                for i in np.arange(0,numprocessors):
+                    overlap[numprocessors-1,i] = float(data.split(', ')[i])
+                #total
+                rest=str(3)
+                command='data=`tail -n+'+linenumber+' '+dbgf+' | head -n'+str(numprocessors)+'`; count=0;for d in $data; do if [ `expr $count % 4` -eq '+rest+' ]; then echo $d", "; fi; count=`expr $count + 1`; done'
+                data = str(parse(command))
+                for i in np.arange(0,numprocessors):
+                    total[numprocessors-1,i] = float(data.split(', ')[i])
     return owned, overlap, total
 
 def writedata(foldername, casename, numthreads, maxproc):
@@ -118,5 +149,5 @@ if __name__ == "__main__":
         os.makedirs(foldername)
 
     for numthreads in np.arange(1,args.maxthreads+1):
-        writedata(foldername, "NORNE_ATW2013",numthreads, args.maxproc)
+        #writedata(foldername, "NORNE_ATW2013",numthreads, args.maxproc)
         writedata(foldername, "SPE9",numthreads, args.maxproc)
